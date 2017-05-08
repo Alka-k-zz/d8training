0093c4ee2c11a8f4d98ab78a6e0363a239da77ef
Alka-k <alkakumari543@gmail.com>
2017-05-08 18:16:56 +0530

    Event API: Tapping into page request process

---
+++ moduleone/src/EventSubscriber/AccessHeaderInjector.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\moduleone\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class AccessHeaderInjector implements EventSubscriberInterface {
+	public static function getSubscribedEvents() {
+		return [
+				KernelEvents::RESPONSE => [
+						'addAccessHeader'
+				]
+		];
+	}
+	public function addAccessHeader(FilterResponseEvent $event) {
+		$response = $event->getResponse ();
+		$response->headers->add ( [
+				'access-control-allow-origin' => '*'
+		] );
+	}
+}
+
